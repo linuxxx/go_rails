30d067bc5c2d8dd2755dd6d88d24c66a1e529980
linuxxx <o.ounix@gmail.com>
2013-07-05 00:02:10 +0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoRails::Application.config.secret_token = 'a7c08da04973e0ef7b5842d32143d96a777372331d93e2708c959686d216445960bbf30718055e5c504c05376ecaf8d4b0bf649aa8ae39eb989bcfc7ab8b4d8c'
